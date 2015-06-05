afc067b8291499c1af45add0fafbe0cd2b1f6976
Banwarth Pierre <pierre.banwarth@e.ujf-grenoble.fr>
2015-06-05 17:14:46 +0200

    retour a une version precedente moin buggé

--- src/Heros.java
+++ src/Heros.java
@@ -2,148 +2,138 @@
 
 
 public class Heros {
-	// GLobal WIn Rate
-	private static int globalWin = 0;
-	private static int globalLoose = 0;
-	// Classe WIN RATE
+	
 	public static int persoWin[] = new int[9];
 	public static int persoLose[] = new int[9];
-	// Matchup Win RATE
-	public static int persoMatchupWin[][] = new int[9][9];
-	public static int persoMatchupLose[][] = new int[9][9];
 	
-	public Heros(){
-		for(int i = 0;i<9;i++){
-			persoMatchupWin[i] = new int[9];
-			persoMatchupLose[i]= new int[9];
-		}
-		
-	}
-	public int getWinMatchup(int s1, int s2){
-		
-		return persoMatchupWin[(s1)][(s2)];
-	}
-	public int getLoseMatchup(int s1, int s2){
-		
-		return persoMatchupLose[(s1)][(s2)];
-	}
-	public int getnbMatchup(int s1, int s2){
-		
-		return persoMatchupLose[s1][s2] + persoMatchupWin[s1][s2];
-	}
-	public String getClasseName(int heronum){
-		switch (heronum) {
-        case 0:
-            return "Rogue";
-        case 1:
-        	return "Paladin";
-        case 2:
-        	return "Druid";
-        case 3:
-        	return "Hunt";
-        case 4:
-        	return "Chaman";
-        case 5:
-        	return "Warrior";
-        case 6:
-        	return "Mage";
-        case 7:
-        	return "Warlock";
-        case 8:
-        	return "Priest";
-    }
-		return "Priest";
-	}
-	public int transition(String heroname){
-		switch (heroname) {
-        case "Valeera Sanguinar":
-            return 0;
-        case "Uther Lightbringer":
-        	return 1;
-        case "Malfurion Stormrage":
-        	return 2;
-        case "Rexxar":
-        	return 3;
-        case "Thrall":
-        	return 4;
-        case "Garrosh Hellscream":
-        	return 5;
-        case "Jaina Proudmoore":
-        	return 6;
-        case "Gul'dan":
-        	return 7;
-        case "Anduin Wrynn":
-        	return 8;
-    }
-		return 0;	
-	}
-	public String transition(int heronum){
-		switch (heronum) {
-        case 0:
-            return "Valeera Sanguinar";
-        case 1:
-        	return "Uther Lightbringer";
-        case 2:
-        	return "Malfurion Stormrage";
-        case 3:
-        	return "Rexxar";
-        case 4:
-        	return "Thrall";
-        case 5:
-        	return "Garrosh Hellscream";
-        case 6:
-        	return "Jaina Proudmoore";
-        case 7:
-        	return "Gul'dan";
-        case 8:
-        	return "Anduin Wrynn";
-    }
-		return "Anduin Wrynn";
-		
-		
-		
-	}
-	public void majRatio(String heroname,String heroadverse, boolean Win){
-                if(Win){
-                	persoMatchupWin[transition(heroname)][transition(heroadverse)]++;
-                	persoWin[transition(heroname)]++;
-                	setGlobalWin(getGlobalWin() + 1);
-                }else{
-                	persoMatchupLose[transition(heroname)][transition(heroadverse)]++;
-                	persoLose[transition(heroname)]++;
-                	setGlobalLoose(getGlobalLoose() + 1);
-               }
-             
-		
-	}
-	public float getRatio(int HeroNum){
-		return ((float)persoWin[HeroNum] / ((float)persoWin[HeroNum] + (float)persoLose[HeroNum]))*100;
-	     
-	}
-	public float getRatio(int HeroNum, int OpponentNum){
-		return ((float)persoMatchupWin[HeroNum][OpponentNum] / ((float)persoMatchupWin[HeroNum][OpponentNum] + (float)persoMatchupLose[HeroNum][OpponentNum]))*100;
-	     
-	}
-	public int getNbgame(int HeroNum){
-	     return persoWin[HeroNum] + persoLose[HeroNum];
+	public void majRatio(String heroname, boolean win){
+	     switch (heroname) {
+	         case "Valeera Sanguinar":
+	             if(win){persoWin[0]++;}else{persoLose[0]++;}
+	             break;
+	         case "Uther Lightbringer":
+	             if(win){persoWin[1]++;}else{persoLose[1]++;}
+	             break;
+	         case "Malfurion Stormrage":
+	             if(win){persoWin[2]++;}else{persoLose[2]++;}
+	             break;
+	         case "Rexxar":
+	             if(win){persoWin[3]++;}else{persoLose[3]++;}
+	             break;
+	         case "Thrall":
+	             if(win){persoWin[4]++;}else{persoLose[4]++;}
+	             break;
+	         case "Garrosh Hellscream":
+	             if(win){persoWin[5]++;}else{persoLose[5]++;}
+	             break;
+	         case "Jaina Proudmoore":
+	             if(win){persoWin[6]++;}else{persoLose[6]++;}
+	             break;
+	         case "Gul'dan":
+	             if(win){persoWin[7]++;}else{persoLose[7]++;}
+	             break;
+	         case "Anduin Wrynn":
+	             if(win){persoWin[8]++;}else{persoLose[8]++;}
+	             break;
+	     }
 	}
 
-	public int getNbgameMatchup(int HeroNum , int opponent){
-	     return persoMatchupWin[HeroNum][opponent] + persoMatchupWin[HeroNum][opponent];
-	}
-	public static int getGlobalWin() {
-		return globalWin;
-	}
-	public static void setGlobalWin(int globalWin) {
-		Heros.globalWin = globalWin;
-	}
-	public static int getGlobalLoose() {
-		return globalLoose;
-	}
-	public static void setGlobalLoose(int globalLoose) {
-		Heros.globalLoose = globalLoose;
-	}
+	public float getRatio(String heroname){
+	     switch (heroname) {
+	         case "Valeera Sanguinar":
+	             return ((float)persoWin[0] / ((float)persoWin[0] + (float)persoLose[0]))*100;
+	         case "Uther Lightbringer":
+	        	 return ((float)persoWin[1] / ((float)persoWin[1] + (float)persoLose[1]))*100; 
+	         case "Malfurion Stormrage":
+	        	 return ((float)persoWin[2] / ((float)persoWin[2] + (float)persoLose[2]))*100; 
+	         case "Rexxar":
+	        	 return ((float)persoWin[3] / ((float)persoWin[3] + (float)persoLose[3]))*100; 
+	         case "Thrall":
+	        	 return ((float)persoWin[4] / ((float)persoWin[4] + (float)persoLose[4]))*100; 
+	         case "Garrosh Hellscream":
+	        	 return ((float)persoWin[5] / ((float)persoWin[5] + (float)persoLose[5]))*100; 	        	
+	         case "Jaina Proudmoore":
+	        	 return ((float)persoWin[6] / ((float)persoWin[6] + (float)persoLose[6]))*100; 	
+	         case "Gul'dan":
+	        	 return ((float)persoWin[7] / ((float)persoWin[7] + (float)persoLose[7]))*100;    
+	         case "Anduin Wrynn":
+	        	 return ((float)persoWin[8] / ((float)persoWin[8] + (float)persoLose[8]))*100; 
 
+	     }
+		return 0;
+	}
+	public float getRatio(int heroname){
+	     switch (heroname) {
+	         case 1:
+	             return ((float)persoWin[0] / ((float)persoWin[0] + (float)persoLose[0]))*100;
+	         case 2:
+	        	 return ((float)persoWin[1] / ((float)persoWin[1] + (float)persoLose[1]))*100; 
+	         case 3:
+	        	 return ((float)persoWin[2] / ((float)persoWin[2] + (float)persoLose[2]))*100; 
+	         case 4:
+	        	 return ((float)persoWin[3] / ((float)persoWin[3] + (float)persoLose[3]))*100; 
+	         case 5:
+	        	 return ((float)persoWin[4] / ((float)persoWin[4] + (float)persoLose[4]))*100; 
+	         case 6:
+	        	 return ((float)persoWin[5] / ((float)persoWin[5] + (float)persoLose[5]))*100; 	        	
+	         case 7:
+	        	 return ((float)persoWin[6] / ((float)persoWin[6] + (float)persoLose[6]))*100; 	
+	         case 8:
+	        	 return ((float)persoWin[7] / ((float)persoWin[7] + (float)persoLose[7]))*100;    
+	         case 9:
+	        	 return ((float)persoWin[8] / ((float)persoWin[8] + (float)persoLose[8]))*100; 
+
+	     }
+		return 0;
+	}
+	public String getNomHero(int i){
+		switch (i) {
+        case 1:
+            return"Valeera Sanguinar";
+        case 2:
+       	 return "Uther Lightbringer";
+        case 3:
+       	 return "Malfurion Stormrage";
+        case 4:
+       	 return "Rexxar"; 
+        case 5:
+       	 return "Thrall"; 
+        case 6:
+       	 return "Gul'dan"; 	        	
+        case 7:
+       	 return "Anduin Wrynn";
+        case 8:
+       	 return "Jaina Proudmoore";    
+        case 9:
+       	 return "Garrosh Hellscream"; 
+
+    }
+	return "";
+	}
+	public int getNbgame(String heroname){
+	     switch (heroname) {
+	         case "Valeera Sanguinar":
+	             return persoWin[0] + persoLose[0];
+	         case "Uther Lightbringer":
+	        	 return persoWin[1] + persoLose[1];
+	         case "Malfurion Stormrage":
+	        	 return persoWin[2] + persoLose[2];
+	         case "Rexxar":
+	        	 return persoWin[3] + persoLose[3]; 
+	         case "Thrall":
+	        	 return persoWin[4] +persoLose[4]; 
+	         case "Garrosh Hellscream":
+	        	 return persoWin[5] + persoLose[5]; 	        	
+	         case "Jaina Proudmoore":
+	        	 return persoWin[6] + persoLose[6];
+	         case "Gul'dan":
+	        	 return persoWin[7] + persoLose[7];    
+	         case "Anduin Wrynn":
+	        	 return persoWin[8] + persoLose[8]; 
+
+	     }
+		return 0;
+	}
 
 
 }
--- src/game.java
+++ src/game.java
@@ -9,21 +9,13 @@ public class game {
 	List<String> cartesjetées = new  ArrayList<String>();
 	List<String> mainDepart = new  ArrayList<String>(); 
 	CarteScore carteScore;
-	private boolean arena = false;
-	private boolean win = false;
-	private String playerName;
-	
+	private Boolean win = false;
 	private String heroJoueur;
 	private  String heroAdverse;
-	private String PlayerClasse;
-	private String OpponentClasse;
 	private int nbcartesdepart = 3;
-	private Heros Herotab;
 	
-	public game(String name, Heros Herotab, String playerName){
+	public game(String name){
 		nomjoueur = name;
-		this.setHerotab(Herotab);	
-		this.playerName = playerName;
 	}
 	public void addCartes(String s){
 		if(s.endsWith(" i")){
@@ -46,7 +38,6 @@ public class game {
 	}
 	// quand on sais la win on ajoute toute les cartes dans la main de depart dans carte score
 	public void setWin(boolean b){
-
 		win = b;
 	}
 	public boolean getWin(){
@@ -96,36 +87,6 @@ public class game {
 			return string.substring(0,string.length()-1);	
 		}
 		else return string;
-	}
-	public boolean isArena() {
-		return arena;
-	}
-	public void setArena(boolean arena) {
-		this.arena = arena;
-	}
-	public Heros getHerotab() {
-		return Herotab;
-	}
-	public void setHerotab(Heros herotab) {
-		Herotab = herotab;
-	}
-	public String getPseudoJoueur() {
-		return playerName;
-	}
-	public void setPseudoJoueur(String pseudoJoueur) {
-		playerName = pseudoJoueur;
-	}
-	public String getPlayerClasse() {
-		return PlayerClasse;
-	}
-	public void setPlayerClasse(String joueurClasse) {
-		PlayerClasse = joueurClasse;
-	}
-	public String getOpponentClasse() {
-		return OpponentClasse;
-	}
-	public void setOpponentClasse(String opponentClasse) {
-		OpponentClasse = opponentClasse;
 	}
 }
 
